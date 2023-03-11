26ae67669c4f4d6978419a315e9dcad7d3c06f71
rahul <rg356403@gmail.com>
2023-03-11 19:08:10 +0530

    backend

--- client/index.js
+++ client/index.js
@@ -1,20 +1,77 @@
-(function () {
+(function(){
 
-    let receiverID;
+	let receiverID;
+	const socket = io();
 
+	function generateID(){
+		return `${Math.floor(Math.random() * 899999 + 100000)}`;
+	}
 
-    function generateID() {
-        return `${Math.floor(Math.random() * 899999 + 100000)}`;
-    }
-
-    document.querySelector("#sender-start-con-btn").addEventListener("click", function () {
-        document.querySelector(".receiver").classList.remove("active");
-        let joinID = generateID();
-        document.querySelector("#join-id").innerHTML = `
+	document.querySelector("#sender-start-con-btn").addEventListener("click",function(){
+		document.querySelector(".receiver").classList.remove("active");
+		let joinID = generateID();
+		document.querySelector("#join-id").innerHTML = `
 		<label>Joining Code</label>
 		<span>${joinID}</span>
 		`;
-        
-    });
+		socket.emit("sender-join", {
+			uid:joinID
+		});
+	});
 
+	socket.on("init",function(uid){
+		receiverID = uid;
+		
+		document.querySelector(".fs-screen").classList.add("active");
+	});
+
+	document.querySelector("#file-input").addEventListener("change",function(e){
+		let file = e.target.files[0];
+		if(!file){
+			return;		
+		}
+		let reader = new FileReader();
+		reader.onload = function(e){
+			let buffer = new Uint8Array(reader.result);
+			let el = document.createElement("div");
+			el.classList.add("item");
+			el.innerHTML = `
+					
+					<div class="filename">${file.name}</div>
+					<div class="progress">0%</div>
+			`;
+			document.querySelector(".files-list").appendChild(el);
+			shareFile({
+				filename: file.name,
+				total_buffer_size:buffer.length,
+				buffer_size:1024,
+			}, buffer, el.querySelector(".progress"));
+		}
+		reader.readAsArrayBuffer(file);
+	});
+
+
+	function shareFile(metadata,buffer,progress_node){
+		console.log(receiverID)
+		socket.emit("file-meta", {
+			uid:receiverID,
+			metadata:metadata
+		});
+		
+		socket.on("fs-share",function(){
+			console.log('this is buffer',buffer)
+			let chunk = buffer.slice(0,metadata.buffer_size);
+			console.log(chunk)
+			buffer = buffer.slice(metadata.buffer_size,buffer.length);
+			progress_node.innerText = ` ${Math.trunc(((metadata.total_buffer_size - buffer.length) / metadata.total_buffer_size * 100))}`+'%';
+			if(chunk.length != 0){
+				socket.emit("file-raw", {
+					uid:receiverID,
+					buffer:chunk
+				});
+			} else {
+				console.log("Sent file successfully");
+			}
+		});
+	}
 })();
--- client/receiver.js
+++ client/receiver.js
@@ -1,22 +1,63 @@
-(function () {
+(function(){
 
-
+	const socket = io();
 	let sender_uid;
 
-	function generateID() {
+	function generateID(){
 		return `${Math.floor(Math.random() * 899999 + 100000)}`;
 	}
 
-	document.querySelector("#receiver-start-con-btn").addEventListener("click", function () {
+	document.querySelector("#receiver-start-con-btn").addEventListener("click",function(){
 		sender_uid = document.querySelector("#join-id").value;
 		console.log(sender_uid)
-		if (sender_uid.length == 0) {
+		if(sender_uid.length == 0){
 			return;
 		}
 		let joinID = generateID();
-
+		socket.emit("receiver-join", {
+			sender_uid:sender_uid,
+			uid:joinID
+		});
+		
 		document.querySelector(".fs-screen").classList.add("active");
 	});
 
+	let fileShare = {};
+
+	socket.on("fs-meta",function(metadata){
+		fileShare.metadata = metadata;
+		fileShare.transmitted = 0;
+		fileShare.buffer = [];
+
+		let el = document.createElement("div");
+		el.classList.add("item");
+		el.innerHTML = `
+			<div class="filename">${metadata.filename}</div>
+			<div class="progress">0%</div>
+		`;
+		document.querySelector(".files-list").appendChild(el);
+
+		fileShare.progrss_node = el.querySelector(".progress");
+
+		socket.emit("fs-start",{
+			uid:sender_uid
+		});
+	});
+
+	socket.on("fs-share",function(buffer){
+		console.log("Buffer", buffer);
+		fileShare.buffer.push(buffer);
+		fileShare.transmitted += buffer.byteLength;
+		fileShare.progrss_node.innerText = `${Math.trunc(fileShare.transmitted / fileShare.metadata.total_buffer_size * 100)}` +'%';
+		if(fileShare.transmitted == fileShare.metadata.total_buffer_size){
+			console.log("Download file: ", fileShare);
+			download(new Blob(fileShare.buffer), fileShare.metadata.filename);
+			fileShare = {};
+		} else {
+			socket.emit("fs-start",{
+				uid:sender_uid
+			});
+		}
+	});
 
 })();
